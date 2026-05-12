3de4317eb5c919e5645fd06b68b5a9f0f92ba82c
Prianto <priantox07@gmail.com>
2026-05-12 09:45:26 +0600

    cmt

--- scripts/check_imports.py
+++
@@ -1,9 +0,0 @@
-try:
-    import sentencepiece
-    import tiktoken
-    import google.protobuf
-    print('imports_ok')
-except Exception as e:
-    import traceback
-    traceback.print_exc()
-    print('import_check_failed:', type(e).__name__, e)
